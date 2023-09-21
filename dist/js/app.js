1675ee7d441d9ed4ecfe185ab47762bd8f195f2a
Victor Kruger <98160719+VicSpy@users.noreply.github.com>
2023-09-20 23:11:17 -0300

    Controller

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,7 @@
-export {};
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
